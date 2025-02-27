92b4159884970a951a1786dd5098861ad5a40ae1
PriyeshIllam <seeujob@gmail.com>
2025-02-27 20:40:18 +0100

    added images added for movies and shows and fix the both routes

--- data/movies.js
+++ data/movies.js
@@ -1,4 +1,4 @@
 module.exports = [
-    { id: 1, title: "Inception", year: 2010, image: "/images/inception.jpg", description: "A mind-bending thriller by Christopher Nolan." },
+    { id: 1, title: "Inception", year: 2010, image: "/images/inception1.jpg", description: "A mind-bending thriller by Christopher Nolan." },
     { id: 2, title: "Interstellar", year: 2014, image: "/images/interstellar.jpg", description: "A space epic exploring time and love." }
 ];
